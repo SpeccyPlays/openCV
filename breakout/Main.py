f304424df63b831dce18d71a10c73aa01712be91
SpeccyPlays <102996742+SpeccyPlays@users.noreply.github.com>
2023-01-20 22:13:56 +0000

    Auto playing breakout clone zx

---
+++ breakout/Main.py
@@ -0,0 +1,45 @@
+## Main
+import pyautogui
+import cv2 as cv
+import numpy as np
+import time
+from windowcapture import WindowCapture
+from opencv_functions import *
+import win32gui, win32ui, win32con
+pyautogui.PAUSE = 0.1
+pyautogui.FAILSAFE = True
+#Colours for bounding squares (reminder it's BGR colours used for openCV)
+yellow = (0, 255, 255)
+blue = (255, 0, 0)
+white = (255, 255, 255)
+pink = (153, 51, 255)
+green = (0, 255, 0)
+#initialise windowcapture class
+wincap = WindowCapture('Fuse')
+cv.namedWindow("Resized_frame", cv.WINDOW_NORMAL)
+cv.resizeWindow("Resized_frame", 400, 300)
+key_left = 'a'
+key_right = 's'
+wincap.set_foreground_window()
+while(1):
+    frame = wincap.get_screenshot()
+    frame, ball_loc = locate_one_object_cv('ball.PNG', frame, pink)
+    frame, paddle_loc = locate_one_object_cv('paddle.PNG', frame, blue)
+    cv.imshow("Resized_frame",frame)
+    ball_x, ball_y = ball_loc
+    paddle_x, paddle_y = paddle_loc
+    #print ('Ball location : ', ball_x, ',', ball_y)
+    #print ('Paddle location : ', paddle_x, ',', paddle_y)
+    if int(paddle_x) < int(ball_x):
+        #print('1st if', paddle_x, ball_x)
+        #wincap.send_key_press(key_right)
+        pyautogui.keyDown(key_right)
+        pyautogui.keyUp(key_right)
+    if int(paddle_x) > int(ball_x):
+        #print('2nd if : ', paddle_x, ball_x)
+        pyautogui.keyDown(key_left)
+        pyautogui.keyUp(key_left)
+    k = cv.waitKey(5) & 0xFF
+    if k == 27:
+        break
+cv.destroyAllWindows()
---
+++ breakout/opencv_functions.py
@@ -0,0 +1,36 @@
+import cv2 as cv
+import numpy as np
+def locate_one_object_cv(filename, img, colour):
+    #copied straight off the docs page
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    template = cv.imread(filename,0)
+    w, h = template.shape[::-1]
+    res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    top_left = max_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+    cv.rectangle(img,top_left, bottom_right, colour, 2)
+    middle = (top_left[0] + top_left[0]) + w / 2
+    return img, (middle, top_left[1])
+
+def locate_multiple_objects_cv(filename, img, colour):
+    #copied off the docs page with minor edits
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    template = cv.imread(filename, 0)
+    w, h = template.shape[::-1]
+    res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
+    threshold = 0.9
+    loc = np.where( res >= threshold)
+    locations = list(zip(*loc[::-1]))
+    #print(filename, ' There are', len(locations), 'locations ', locations)
+    for pt in zip(*loc[::-1]):
+        cv.rectangle(img, pt, (pt[0] + w, pt[1] + h), colour, 2)
+    return img, locations
+
+def loop_list_and_draw_rectangles(img, the_list):
+    for item in the_list:
+        draw_rectangle(img, item)
+
+def draw_rectangle(img, item_to_draw):
+    top_left_x, top_left_y, width, height = item_to_draw
+    cv.rectangle(img,(top_left_x, top_left_y),(top_left_x + width, top_left_y + height),(255,255,255),3)

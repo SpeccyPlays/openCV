8ce2733f495e2df9b3df0d0efe7478765e966f8e
SpeccyPlays <102996742+SpeccyPlays@users.noreply.github.com>
2023-01-21 18:40:49 +0000

    This is 'good enough'

--- breakout/Main.py
+++ breakout/Main.py
@@ -5,7 +5,7 @@ import numpy as np
 from windowcapture import WindowCapture
 from opencv_functions import *
 import win32gui, win32ui, win32con
-pyautogui.PAUSE = 0.02
+pyautogui.PAUSE = 0.01
 pyautogui.FAILSAFE = True
 #Colours for bounding squares (reminder it's BGR colours used for openCV)
 yellow = (0, 255, 255)
@@ -30,10 +30,10 @@ while(1):
     #print ('Ball location : ', ball_x, ',', ball_y)
     #print ('Paddle location : ', paddle_x, ',', paddle_y)
     #Change the plus and minus value - lower means more twitchy but can get to the ball quick enough
-    if int(ball_x) > (int(paddle_x) + 2):
+    if int(ball_x) > (int(paddle_x) + 5):
         pyautogui.keyUp(key_left)
         pyautogui.keyDown(key_right)
-    if int(ball_x) < (int(paddle_x) - 2):
+    if int(ball_x) < (int(paddle_x) - 5):
         pyautogui.keyUp(key_right)
         pyautogui.keyDown(key_left)  
     k = cv.waitKey(5) & 0xFF
